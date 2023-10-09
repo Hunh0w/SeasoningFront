f8366b92ec51d871ac3a5011a2b21f823c77fbdb
Adrien Raimbault <adrien.raimbault@etu.umontpellier.fr>
2023-10-09 17:27:46 +0200

    feat(home/offers): add home/offers tab

---
+++ components/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import MainMenu from './MainMenu';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
