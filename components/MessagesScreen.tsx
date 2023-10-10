3107b1c6db8e1b1ff2e3a16da25cc1055ae6824c
Hunh0w <vincent.font@etu.umontpellier.fr>
2023-10-10 14:57:49 +0200

    feat: messages screen

--- components/MessagesScreen.tsx
+++ components/MessagesScreen.tsx
@@ -1,11 +1,69 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { Image, Drawer, View, Text, LoaderScreen } from 'react-native-ui-lib';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+interface MessageElement {
+  id: number,
+  avatar: string,
+  name: string,
+  lastMessage: string
+}
 
 export default function MessagesScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Messages</Text>
-    </View>
+
+  const [messages, setMessages] = useState<MessageElement[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const msgs: MessageElement[] = [];
+    for(let i = 0; i < 31; i++){
+      msgs.push({
+        id: i,
+        avatar: "https://static-00.iconduck.com/assets.00/user-avatar-icon-512x512-vufpcmdn.png",
+        name: "John Cena "+(i+1),
+        lastMessage: "C'est quand que jte pete la gueule ?"
+      })
+    }
+    setMessages(msgs);
+    setLoading(false);
+  }, []);
+
+  const hideElement = (element: MessageElement) => {
+    setLoading(true);
+    //TODO API Call
+
+    const elements = messages.filter((obj) => obj.id != element.id);
+
+    
+    setMessages(elements);
+    setLoading(false);
+   
+  }
+
+  return (<>
+    {loading && <LoaderScreen message='Loading...' overlay/>}
+    {!loading &&
+      <GestureHandlerRootView>
+        <ScrollView>
+          {messages.map((element,i) => {
+            return <Drawer key={i} leftItem={{text: 'Hide', background: Colors.red30, onPress: () => hideElement(element)}}
+
+            >
+              <View bg-white style={{ height: 80, display: "flex", flexDirection: "row", alignItems: "center"}}>
+                <Image width={50} height={50} style={{marginHorizontal: 10}} source={{uri: element.avatar}} />
+                <View style={{display: "flex", justifyContent: "center"}}>
+                  <Text style={{fontWeight: "bold"}}>{element.name}</Text>
+                  <Text>{element.lastMessage}</Text>
+                </View>
+              </View>
+            </Drawer>
+          })}
+        </ScrollView>
+      </GestureHandlerRootView>
+    }
+    </>
   );
 }
 
--- tabs/MessagesStackScreen.tsx
+++ tabs/MessagesStackScreen.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react'
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MessagesScreen from '../components/MessagesScreen';
+import { FlatList, View } from 'react-native';
+import { ListItem } from 'react-native-ui-lib';
 
 const MessagesStack = createNativeStackNavigator();
 
 export default function MessagesStackScreen() {
+
   return (
-    <MessagesStack.Navigator>
-      <MessagesStack.Screen name="Messages" component={MessagesScreen} />
-      {/* Conversation : <MessagesStack.Screen name="Details" component={ConversationScreen} /> */}
-    </MessagesStack.Navigator>
+      <MessagesStack.Navigator>
+        <MessagesStack.Screen name="Messages" component={MessagesScreen} />
+      </MessagesStack.Navigator>
   );
 }
