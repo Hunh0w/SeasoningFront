d5f5469d19e467288129b4806dcd6357959787cc
Adrien Raimbault <adrien.raimbault@etu.umontpellier.fr>
2023-10-30 17:11:41 +0100

    feat: add authNavigator for unauthentified user interaction

--- App.tsx
+++ App.tsx
@@ -1,66 +1,22 @@
-import * as React from "react";
 
-import { StyleSheet } from "react-native";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+// Store imports
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
-import EmployersStackScreen from "./tabs/EmployersStackScreen";
-import HomeStackScreen from "./tabs/HomeStackScreen";
-import MessagesStackScreen from "./tabs/MessagesStackScreen";
+// Import paper to user react paper components
+import { PaperProvider } from "react-native-paper";
 
-const Tab = createMaterialBottomTabNavigator();
+import Navigation from "./Navigation";
 
-export default function App() {
+const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="offersTab"
-        activeColor="#291efc"
-        inactiveColor="#3e2465"
-        barStyle={{ backgroundColor: "#ffff" }}
-      >
-        <Tab.Screen
-          name="HomeTab"
-          component={HomeStackScreen}
-          options={{
-            tabBarLabel: "Offres",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="home" color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="EmployersTab"
-          component={EmployersStackScreen}
-          options={{
-            tabBarLabel: "Employeurs",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons
-                name="account-group"
-                color={color}
-                size={26}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="MessagesTab"
-          component={MessagesStackScreen}
-          options={{
-            tabBarLabel: "Messages",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons
-                name="android-messages"
-                color={color}
-                size={26}
-              />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    // Store provider
+    <Provider store={store}>
+      <PaperProvider>
+        <Navigation />
+      </PaperProvider>
+    </Provider>
   );
-}
+};
 
-const styles = StyleSheet.create({});
+export default App;
---
+++ Navigation.tsx
@@ -0,0 +1,97 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// Import useSelector to retrieve user info from store
+import { useSelector } from "react-redux";
+
+// Import state type
+import { RootState } from "./store/store";
+
+// Import screens
+import EmployersStackScreen from "./tabs/EmployersStackScreen";
+import HomeStackScreen from "./tabs/HomeStackScreen";
+import MessagesStackScreen from "./tabs/MessagesStackScreen";
+import LandingScreen from "./components/LandingScreen";
+import SigninScreen from "./components/SignInScreen";
+
+const Tab = createMaterialBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+// Navigator for non-connected users (landing, signIn, signUp pages)
+function AuthNavigator() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Landing" component={LandingScreen} />
+      <Stack.Screen name="Signin" component={SigninScreen} />
+    </Stack.Navigator>
+  );
+}
+
+// Navigator for connected users (Offers, Employers and Messages)
+function AppNavigator() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Offers"
+      activeColor="#291efc"
+      inactiveColor="#3e2465"
+      barStyle={{ backgroundColor: "#ffff" }}
+    >
+      <Tab.Screen
+        name="Offers"
+        component={HomeStackScreen}
+        options={{
+          tabBarLabel: "Offres",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Employeurs"
+        component={EmployersStackScreen}
+        options={{
+          tabBarLabel: "Employeurs",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="account-group"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Messages"
+        component={MessagesStackScreen}
+        options={{
+          tabBarLabel: "Messages",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="android-messages"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+// Check user authentication FRONT side
+export default function Navigation() {
+  // Check user auth
+  const user = useSelector((state: RootState) => state.user);
+  // console.log("User state => ", user);
+  return (
+    <NavigationContainer>
+      {user.connected ? <AppNavigator /> : <AuthNavigator />}
+    </NavigationContainer>
+  );
+}
