3d5d50eeced6264df86440f85d05742175a50cca
Asstra <95973267+Assstra@users.noreply.github.com>
2023-10-30 17:40:41 +0100

    Feat/employers (#5)
    * feat(employers): add employers tab
    
    * misc(employers): add employers json file for testing and future API
    
    * fix(employers): change name to avoid error and delete shit

--- components/EmployersScreen.tsx
+++ components/EmployersScreen.tsx
@@ -1,11 +1,106 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useEffect, useState } from "react";
+import { StyleSheet, ScrollView, Linking } from "react-native";
+
+import { Avatar, Divider } from "react-native-paper";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { View, Text, Drawer, LoaderScreen } from "react-native-ui-lib";
+
+// TEMPORARY (fetch json for employers)
+import data from "../employers.json";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+interface Employer {
+  id: number;
+  name: string;
+  surname: string;
+  avatar: string;
+  company: string;
+  email: string;
+}
+
+type Props = {
+  employer: Employer;
+};
+
+export function Employer({ employer }: Props) {
+  const sendMessage = (employer: Number) => {
+    // TODO : API Call to open a new conversation between U and employer
+    // TODO : redirect to messages tab
+  };
+
+  return (
+    <>
+      <Drawer>
+        <View
+          bg-white
+          style={{
+            height: 80,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <View style={{ marginHorizontal: 10 }}>
+            <Avatar.Image size={50} source={{ uri: employer.avatar }} />
+          </View>
+
+          <View style={{ display: "flex", justifyContent: "center" }}>
+            <Text style={{ fontWeight: "bold" }}>
+              {employer.surname} {employer.name}
+            </Text>
+            <Text style={{ width: 180 }}>{employer.company}</Text>
+          </View>
+          <View style={{ display: "flex", justifyContent: "center" }}>
+            <MaterialCommunityIcons
+              onPress={() => {
+                sendMessage(employer.id);
+              }}
+              name="message-arrow-right"
+              color="#291efc"
+              size={26}
+            />
+          </View>
+          <View
+            style={{ display: "flex", justifyContent: "center", padding: 20 }}
+          >
+            <MaterialCommunityIcons
+              onPress={() => {
+                Linking.openURL("mailto:" + employer.email);
+              }}
+              name="mail"
+              color="#291efc"
+              size={32}
+            />
+          </View>
+        </View>
+        <Divider />
+      </Drawer>
+    </>
+  );
+}
 
 export default function EmployersScreen() {
+  const [employers, setEmployers] = useState(data);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // TODO : API Call to retrieve employers from json file
+    setLoading(false);
+  }, []);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Employers</Text>
-    </View>
+    <>
+      {loading && <LoaderScreen message="Loading..." overlay />}
+      {!loading && (
+        <GestureHandlerRootView>
+          <ScrollView>
+            {employers.map((employer, index) => (
+              <Employer key={index} employer={employer} />
+            ))}
+          </ScrollView>
+        </GestureHandlerRootView>
+      )}
+    </>
   );
 }
 
--- tabs/EmployersStackScreen.tsx
+++ tabs/EmployersStackScreen.tsx
@@ -1,13 +1,19 @@
-import * as React from 'react'
+import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import EmployersScreen from '../components/EmployersScreen';
+import EmployersScreen from "../components/EmployersScreen";
 
 const EmployersStack = createNativeStackNavigator();
 
 export default function EmployersStackScreen() {
   return (
     <EmployersStack.Navigator>
-      <EmployersStack.Screen name="Employeurs" component={EmployersScreen} />
+      <EmployersStack.Screen
+        name="EmployersStackScreen"
+        options={{
+          title: "Employeurs"
+        }}
+        component={EmployersScreen}
+      />
       {/* For employer profile details/overview <EmployersStack.Screen name="EmployersProfile" component={EmployerProfileScreen} /> */}
     </EmployersStack.Navigator>
   );
