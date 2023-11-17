5e7e5d31cd332ffe83d17be506ed6657ab60f375
Adrien Raimbault <adrien.raimbault@etu.umontpellier.fr>
2023-11-17 22:22:24 +0100

    misc(validators): add validators for signIn & signUp

--- components/misc/validators.tsx
+++ components/misc/validators.tsx
@@ -1,4 +1,5 @@
 import * as Yup from "yup";
+import { genders } from "./genre";
 
 export function loginValidationScheme() {
   return Yup.object().shape({
@@ -10,4 +11,35 @@ export function loginValidationScheme() {
       .min(6, "Password must be at least 6 characters")
       .max(40, "Password must not exceed 40 characters"),
   });
+}
+
+export function register1ValidationScheme() {
+  return Yup.object().shape({
+    email: Yup.string().required("Email is required").email("Invalid format"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters")
+      .max(40, "Password must not exceed 40 characters"),
+    confirmPassword: Yup.string()
+      .required("Password confirmation is required")
+      .oneOf([Yup.ref("password"), ""], "Confirm password does not match"),
+  });
+}
+
+export function register2ValidationScheme() {
+  return Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    surname: Yup.string().required("Surname is required"),
+    birth: Yup.date().required("Birth date required"),
+    genre: Yup.string().oneOf(genders, "Genre doesn't match any")
+  })
+}
+
+export function register3ValidationScheme() {
+  return Yup.object().shape({
+    phone: Yup.number().required("Phone number is required"),
+    Address: Yup.string().required("Address is required"),
+    city: Yup.string().required("City is required"),
+    country: Yup.string().required("Country is required"),
+  })
 }
