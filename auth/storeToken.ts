7ea776607b4525937a5d7a68e0ca221ed075ccc4
Adrien Raimbault <adrien.raimbault@etu.umontpellier.fr>
2024-03-01 11:27:16 +0100

    misc: add type to token

--- auth/storeToken.ts
+++ auth/storeToken.ts
@@ -1,9 +1,9 @@
 import { useAsyncStorage } from "@react-native-async-storage/async-storage";
 
 export interface SSOToken {
-  accessToken: string,
-  idToken: string,
-  refreshToken: string | undefined,
+  accessToken: string;
+  idToken: string;
+  refreshToken: string;
 }
 
 export function storeToken() {
@@ -11,11 +11,11 @@ export function storeToken() {
 
   const getToken = async () => {
     const tokensStringify = await getItem();
-    return tokensStringify ? (JSON.parse(tokensStringify)) : null;
+    return tokensStringify ? (JSON.parse(tokensStringify) as SSOToken) : null;
   };
   const setToken = async (tokens: SSOToken) =>
     await setItem(JSON.stringify(tokens));
   const removeToken = async () => await removeItem();
 
-  return {getToken, setToken, removeToken}
+  return { getToken, setToken, removeToken };
 }
